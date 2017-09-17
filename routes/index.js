8936cc781a90ecdfcbd612ae1ee8f5919feaab04
Paul Carrillo <paulcarrillo05@gmail.com>
2017-09-17 16:18:34 -0600

    datase is connected

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+  const express = require('express');
+  const router = express.Router();
+  const storeController = require('../controllers/storeController');
+
+
+  router.get('/', storeController.myMiddleware, storeController.homePage);
+
+  module.exports = router;
