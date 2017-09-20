18e052e15061597351c2c2b49b0b47b89320c2ff
Paul Carrillo <paulcarrillo05@gmail.com>
2017-09-19 23:12:58 -0600

    routes configured for store, and store card was created in pug

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,9 +1,23 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'Wes';
-  next();
-}
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+}
+
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save()
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  //1. Query the database for a list of all stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
 };
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@
   const router = express.Router();
   const storeController = require('../controllers/storeController');
 
+  const { catchErrors } = require('../handlers/errorHandlers');
 
-  router.get('/', storeController.myMiddleware, storeController.homePage);
+  router.get('/', catchErrors(storeController.getStores));
+  router.get('/stores', catchErrors(storeController.getStores));
+  router.get('/add', storeController.addStore);
+  router.post('/add', catchErrors(storeController.createStore));
 
   module.exports = router;
