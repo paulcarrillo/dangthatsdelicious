a9db9a882327f4b2a615b20508791559274c70cf
Paul Carrillo <paulcarrillo05@gmail.com>
2017-09-26 16:32:21 -0600

    Image option added, using a static map to display the location entered on google maps in an image as part of the layout

--- models/Store.js
+++ models/Store.js
@@ -31,7 +31,8 @@ const storeSchema = new mongoose.Schema({
       type: String,
       required: 'You must supply a address!'
     }
-  }
+  },
+  photo: String
 });
 
   storeSchema.pre('save', function(next) {
